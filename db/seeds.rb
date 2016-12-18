20251b59d073c514df669bba98466f6f5f747250
Techforchange <arleneperezsb@gmail.com>
2016-12-17 23:01:22 -0800

    Creates SAT objects for all 4 SAT test dates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,72 @@
 # require 'mechanize'
 require 'open-uri'
 #Scraping for SAT locations
-page = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201701&country=US&state=CA&city="))
+page1 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201701&country=US&state=CA&city="))
 
-sat_objects =  page.css('div.test-center-results-row')[0].text
- all_objects = page.css('div.test-center-results-row')
- schools_array = []
- all_objects.each do |school|
+ january_nokogiri = page1.css('div.test-center-results-row')
+ january_locations = []
+
+ january_nokogiri.each do |school|
    schools = {}
    schools[:name] = school.css('h3').text
    schools[:address] = school.css('.test-center-result-value').text.split(" ").join(" ")
-   schools_array << schools
+   january_locations << schools
  end
 
 
- schools_array.each do |school|
-   Sat.create(location_name: school[:name], address: school[:address] )
+ january_locations.each do |school|
+   Sat.create(location_name: school[:name], address: school[:address], date: 20170121, completed: false )
  end
 
+################################################################################
+page2 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201703&country=US&state=CA&city="))
+march_nokogiri = page1.css('div.test-center-results-row')
+march_locations = []
+
+march_nokogiri.each do |school|
+  schools = {}
+  schools[:name] = school.css('h3').text
+  schools[:address] = school.css('.test-center-result-value').text.split(" ").join(" ")
+  march_locations << schools
+end
+
+
+march_locations.each do |school|
+  Sat.create(location_name: school[:name], address: school[:address], date: 20170311, completed: false )
+end
+################################################################################
+page3 = page2 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201705&country=US&state=CA&city="))
+may_nokogiri = page1.css('div.test-center-results-row')
+may_locations = []
+
+may_nokogiri.each do |school|
+  schools = {}
+  schools[:name] = school.css('h3').text
+  schools[:address] = school.css('.test-center-result-value').text.split(" ").join(" ")
+  may_locations << schools
+end
+
+
+may_locations.each do |school|
+  Sat.create(location_name: school[:name], address: school[:address], date: 20170506, completed: false )
+end
+
+################################################################################
+page4 = page2 = Nokogiri::HTML(open("https://collegereadiness.collegeboard.org/api/v1/test-center-search?test_date=201706&country=US&state=CA&city="))
+june_nokogiri = page1.css('div.test-center-results-row')
+june_locations = []
+
+june_nokogiri.each do |school|
+  schools = {}
+  schools[:name] = school.css('h3').text
+  schools[:address] = school.css('.test-center-result-value').text.split(" ").join(" ")
+  june_locations << schools
+end
+
+
+june_locations.each do |school|
+  Sat.create(location_name: school[:name], address: school[:address], date: 20170603, completed: false )
+end
 # sat_locations = Mechanize.new
 #
 # page = sat_locations.get('https://collegereadiness.collegeboard.org/sat/register/find-test-centers')
@@ -41,5 +90,3 @@ sat_objects =  page.css('div.test-center-results-row')[0].text
     #   link = entry.css('p.title>a')[0]['href']
     #   entriesArray << Entry.new(title, link)
     # end
-
-  
