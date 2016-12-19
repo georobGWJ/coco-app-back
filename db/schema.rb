49b0fa8839fd23561123cd30331b62cf02927116
Robert Turner Jr <georob23@gmail.com>
2016-12-19 09:20:19 -0800

    Fix typo in migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218230755) do
+ActiveRecord::Schema.define(version: 20161219163033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20161218230755) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
   end
 
   create_table "requirements", force: :cascade do |t|
